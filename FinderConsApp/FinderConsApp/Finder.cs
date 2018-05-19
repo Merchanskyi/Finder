f16192ba5902cfb0671c350e61f55e980acce168
Ruslan <rusya998@gmail.com>
2018-05-19 20:39:50 +0300

    Complete

---
+++ FinderConsApp/FinderConsApp/Finder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FinderConsApp
+{
+    public class Finder
+    {
+        private List<string> _result = new List<string>();
+        private object _lock = new object();
+
+        public List<string> StartScanDirectory(string baseDirectory)
+        {
+            var task = ScanDirectoryAsync(baseDirectory);
+
+            var suspend = false;
+
+            while (!task.IsCompleted)
+            {
+                if (Console.KeyAvailable)
+                {
+                    var pressButton = Console.ReadKey();
+                    if (pressButton.Key == ConsoleKey.Spacebar)
+                    {
+                        if (suspend)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Поиск продолжен, нажмите пробел чтобы поставить на паузу");
+                            Console.ResetColor();
+                            Thread.Sleep(400);
+                            Monitor.Exit(_lock);
+                        }
+                        else
+                        {
+                            Monitor.Enter(_lock);
+                            Thread.Sleep(300);
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Поиск в ожидании, нажмите пробел чтобы продолжить");
+                            Console.ResetColor();
+                        }
+                    }
+                    suspend = !suspend;
+                }
+                Thread.Sleep(100);
+            }
+            return _result;
+        }
+
+        private Task ScanDirectoryAsync(string baseDirectory)
+        {
+            return Task.Run(async () =>
+            {
+                Monitor.Enter(_lock);
+                Monitor.Exit(_lock);
+
+                if (!Directory.Exists(baseDirectory))
+                {
+                    return;
+                }
+
+                try
+                {
+                    Console.WriteLine(baseDirectory);
+                    _result.AddRange(Directory.GetFiles(baseDirectory));
+
+                    await Task.WhenAll(Directory.GetDirectories(baseDirectory).Select(x => ScanDirectoryAsync(x)));
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e.Message);
+                    Console.ResetColor();
+                }
+            });
+        }
+    }
+}
---
+++ FinderConsApp/FinderConsApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+namespace FinderConsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Finder finder = new Finder();
+
+            Console.Write("Введите путь для поиска (Пример: C:\\): ");
+            var dir = Console.ReadLine();
+
+            if (dir != null)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Чтобы возобновить либо остановить поиск нажмите пробел");
+                Console.ResetColor();
+                Thread.Sleep(2000);
+            }
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            List<string> location = finder.StartScanDirectory(@dir);
+
+            sw.Stop();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Конец поиска");
+            Console.WriteLine("Кол-во файлов: " + location.Count);
+            Console.WriteLine("Время поиска: " + sw.Elapsed);
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
